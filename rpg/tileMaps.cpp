4e05cc1b3e2b9f5271b7b4fdceddc4032409142e
t o <finalfantasytimothy@gmail.com>
2018-08-29 17:00:02 -0700

    Tile Map work

--- rpg/tileMaps.cpp
+++ rpg/tileMaps.cpp
@@ -1,16 +1,56 @@
 #include "tileMaps.h"
 
-
-
 TileMaps::TileMaps()
 {
 }
 
 void TileMaps::load(std::string fileName, SDL_Renderer *rend)
 {
+	fin.open("Data/Maps/Test_Map.json");
+	tile_map = json::parse(fin);
+
+	filePath = tile_map["File_Path"].get<std::string>();
+	tileAmount = tile_map["Tile_Amount"].get<int>();
+	tileHeight = tile_map["Tile_Height"].get<int>();
+	tileWidth = tile_map["Tile_Width"].get<int>();
+	mapHeight = tile_map["Map_Height"].get<int>();
+	mapWidth = tile_map["Map_Width"].get<int>();
 
 }
 
+bool TileMaps::isValidTile()
+{
+	return false;
+}
+
+void TileMaps::drawMap(SDL_Renderer *rend)
+{
+	int i = 0;
+	int curTile = 0;
+	for (auto& it : tile_map["Layer1"])
+	{
+		i++;
+		curTile = it;
+
+		map[i] = new Tile(x, y, curTile);
+	}
+
+
+	SDL_Rect src;
+	src.x = rows;
+	src.y = cols;
+	src.w = tileWidth;
+	src.h = tileHeight;
+
+	SDL_Rect dest;
+	dest.x = x;
+	dest.y = y;
+	dest.w = src.w;
+	dest.h = src.h;
+
+	SDL_RenderCopy(rend, renderedMap, &src, &dest);
+
+}
 
 TileMaps::~TileMaps()
 {
--- rpg/tileMaps.h
+++ rpg/tileMaps.h
@@ -2,22 +2,57 @@
 #include <SDL.h>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <SDL_image.h>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+class Tile
+{
+private:
+	int x;
+	int y;
+	std::string tileType;
+	SDL_Rect srcTile;
+
+public:
+	Tile()
+	{
+		x = 0;
+		y = 0;
+		tileType = nullptr;
+	}
+
+	Tile(int dx, int dy, std::string tile)
+	{
+		x = dx;
+		y = dy;
+		tileType = tile;
+	}
+
+};
 
 class TileMaps
 {
 private:
-	int row;
-	int col;
-	int w;
-	int h;
-	int layers;
+	std::string filePath;
+	int layers, tileAmount;
+	int x, y;
+	int rows, cols;
+	int tileWidth, tileHeight, mapHeight, mapWidth;
+	std::ifstream fin;
+	std::string type;
 	SDL_Texture *renderedMap;
 	std::string fileName;
+	json tile_map;
+	Tile** map;
 
 public:
 	TileMaps();
 	void load(std::string fileName, SDL_Renderer *rend);
+	bool isValidTile();
+	void drawMap(SDL_Renderer *rend);
 	~TileMaps();
 };
 
