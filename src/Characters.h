b54cf96e2229d0dcf0bc93b2b2c696515a75884e
Timothy O'Barr <finalfantasytimothy@gmail.com>
2018-11-01 12:26:23 -0700

    Added Apply Job Class and var
    Probably gonna use canApply in that context as well with some skills being less effective or being learn able based on Job

--- src/Characters.h
+++ src/Characters.h
@@ -13,12 +13,15 @@ private:
 	std::ifstream fin;
 	Creature *party;
 	json Heros;
-
+	JobClass jobAccess;
 
 public:
 	Characters();
 	bool openFile();
 	bool canApplySkill();
+	void applyJob();  // Should this be a seperate func or part of createCharaters I could make a Job switch
+			 // Eleminate the Character copying their stats and such replacing them with a new character instead
+			// Or I could go the sensicle and not stupif AF route and actually apply the Job's the way they should be
 	Characters *createCharacters();
 
 	~Characters();
